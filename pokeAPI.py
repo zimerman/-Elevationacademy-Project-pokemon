1a2b82c7d20f9d054add2ef136caccc4f432bef6
zimerman <zr@sem-wolf.org>
2020-08-04 00:31:15 +0300

    delete and add routes

--- pokeAPI.py
+++ pokeAPI.py
@@ -24,7 +24,7 @@ def get_owner_by_pokemon(name_pokemon):
     query = """SELECT name from trainers
              WHERE name in
              (SELECT ownedby.name_trainer
-              FROM pokemon JOIN ownedby 
+              FROM pokemon JOIN ownedby
               ON pokemon.id = ownedby.id_pokemon AND pokemon.name = '{}')""".format(name_pokemon)
     try:
         with connection.cursor() as cursor:
@@ -37,13 +37,13 @@ def get_owner_by_pokemon(name_pokemon):
         return json.dumps({"ERROR": str(e)}), 500
 
 
-@app.route('/pokemons_owner/<name_owner>')
+@app.route('/pokemons_owner/<name_trainer>')
 def get_pokemon_by_owner(name_trainer):
 
     query = """SELECT name from pokemon
             WHERE id in
-            (SELECT ownedby.id_pokemon 
-            FROM trainers JOIN ownedby 
+            (SELECT ownedby.id_pokemon
+            FROM trainers JOIN ownedby
             ON ownedby.name_trainer = trainers.name AND trainers.name = '{}')""".format(name_trainer)
     try:
         with connection.cursor() as cursor:
@@ -58,9 +58,9 @@ def get_pokemon_by_owner(name_trainer):
 
 @app.route('/pokemons_type/<type>')
 def get_pokemons_by_type(type):
-    query = """"SELECT id 
+    query = """"SELECT id
     FROM pokemon JOIN pokemontype
-    ON pokemon.id = pokemontype.id_pokemon AND name_type = '{}'""".format(
+    ON pokemon.id = pokemontype.id_pokemon AND pokemontype.name_type = '{}'""".format(
         type
     )
     try:
@@ -72,6 +72,70 @@ def get_pokemons_by_type(type):
         return json.dumps(result)
     except Exception as e:
         return json.dumps({"ERROR": str(e)}), 500
+
+
+@app.route('/delete_pokemon_trainer', methods=['DELETE'])
+def delete_pokemon_and_trainer():
+    pokemon_trainer = request.get_json()
+    mandatory_params = ["pokemon", "trainer"]
+    missing_params = [params for params in mandatory_params if not pokemon_trainer.get(params)]
+    if missing_params:
+        return {"Error": f"params {missing_params} are missing"}, 400
+    query = "DELETE FROM ownedby WHERE name_trainer = '{}' AND id_pokemon = (SELECT id FROM pokemon WHERE name = '{}')".format(
+        pokemon_trainer.get('pokemon'),
+        pokemon_trainer.get('trainer')
+    )
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            connection.commit()
+            return {"Status": "Succes, deleted pokemon and trainer"}, 201
+    except Exception as error:
+        code, meesage = error.args
+        return {"Error": "This pokemon and trainer not exist"}, 409
+    # except Exception as e:
+    #     return json.dumps({"ERROR": str(e)}), 500
+
+
+@app.route('/add_pokemon', methods=["POST"])
+def add_pokemon():
+    pokemon_to_add = request.get_json()
+    mandatory_fields = ["id", "name", "height", "weight"]
+    missing_params = [fields for fields in mandatory_fields if not pokemon_to_add.get(fields)]
+    if missing_params:
+        return {"Error": f"fields {missing_params} are missing"}, 400
+    list_types = pokemon_to_add.get("type")
+    query = "INSERT into pokemon values ({},'{}',{},{})".format(
+        pokemon_to_add.get("id"),
+        pokemon_to_add.get("name"),
+        pokemon_to_add.get("height"),
+        pokemon_to_add.get("weight")
+    )
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query)
+            connection.commit()
+            return {"Status": "Succes, Added pokemon"}, 201
+    except Exception as error:
+        code, meesage = error.args
+        return {"Error": "This pokemon already exist"}, 409
+    except Exception as e:
+        return json.dumps({"ERROR": str(e)}), 500
+    for type in list_types:
+        query2 = "INSERT into pokemontype values({}, '{})".format(
+            pokemon_to_add.get("id"),
+            type
+        )
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
+                return {"Status": "Succes, Added pokemon"}, 201
+        except Exception as error:
+            code, meesage = error.args
+            return {"Error": "This pokemon and trainer not exist"}, 409
+        except Exception as e:
+            return json.dumps({"ERROR": str(e)}), 500
 
 
 port_number = 3000
