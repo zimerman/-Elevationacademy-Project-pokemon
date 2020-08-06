a872a3db85b1bd95b14e023a679baa45eb7b709e
zimerman <zr@sem-wolf.org>
2020-08-06 22:18:15 +0300

    query trainer table

--- type_data.py
+++ type_data.py
@@ -1,6 +1,4 @@
-import pymysql
 import json
-from pymysql import IntegrityError
 
 from conect import connection
 
@@ -19,10 +17,11 @@ def get_pokemon_by_type(type):
 
 def add_to_type(list_types):
     for type in list_types:
-        query = "INSERT into type values('{}')".format(type)
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            connection.commit()
+        if not is_exist_type(type):
+            query = "INSERT into type values('{}')".format(type)
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+                connection.commit()
 
 
 def get_types(name_pokemon):
@@ -35,9 +34,39 @@ def get_types(name_pokemon):
     return json.dumps(result.get("name_type"))
 
 
+def get_types_feed(name_pokemon):
+    query = "SELECT name_type FROM pokemontype JOIN pokemon ON pokemontype.id_pokemon = pokemon.id AND name = '{}'".format(name_pokemon)
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+    result = cursor.fetchone()
+    if not result:
+        return result
+    return result.get("name_type")
+
+
 def add_to_pokemontype(id_, type):
     query2 = "INSERT into pokemontype values({}, '{}')".format(
         id_, type)
     with connection.cursor() as cursor:
         cursor.execute(query2)
         connection.commit()
+
+
+def add_to_pokemontype_list(id_, types):
+    for type in types:
+        query2 = "INSERT into pokemontype values({}, '{}')".format(
+            id_, type)
+        with connection.cursor() as cursor:
+            cursor.execute(query2)
+            connection.commit()
+
+
+def is_exist_type(type):
+    query = "SELECT name FROM type WHERE name = '{}'".format(type)
+    with connection.cursor() as cursor:
+        cursor.execute(query)
+        result = cursor.fetchone()
+    if result:
+        return True
+    return False
+
